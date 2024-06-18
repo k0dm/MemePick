a8c40c2d71f2c63ef1fa0b1bc8c1e7f4c3479543
k0dm <fighttournament2@gmail.com>
2024-06-18 12:22:33 +0300

    MP-06 Firebase Module

--- core/firebase/build.gradle.kts
+++ core/firebase/build.gradle.kts
@@ -1,22 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
-    namespace = "com.bugbender.memepick"
+    namespace = "com.bugbender.mempick.core.firebase"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.bugbender.memepick"
         minSdk = 29
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -35,38 +31,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
 
-    //modules
-    implementation(project(":core:presentation"))
-    implementation(project(":core:theme"))
-    implementation(project(":core:data"))
-    implementation(project(":core:firebase"))
-    implementation(project(":features:memes"))
-    implementation(project(":data:memes-api"))
-    implementation(project(":data:memes-imp"))
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 
-    //hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics")
 
     //base libs
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-}
-
-// Allow references to generated code
-kapt {
-    correctErrorTypes = true
 }
